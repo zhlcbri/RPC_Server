44766a2f8e09efad986b76c92ae0018406fd7eb6
Lichen Zhang <liz100@UCSD.EDU>
2018-06-08 20:52:27 -0700

    recompiled

--- Server/Example1_s.c
+++ Server/Example1_s.c
@@ -7,8 +7,8 @@
 /* at Mon Jan 18 19:14:07 2038
  */
 /* Compiler settings for Example1.idl:
-    Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.01.0622 
-    protocol : dce , ms_ext, app_config, c_ext, robust
+    Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 8.01.0622 
+    protocol : all , ms_ext, app_config, c_ext, robust
     error checks: allocation ref bounds_check enum stub_data 
     VC __declspec() decoration level: 
          __declspec(uuid()), __declspec(selectany), __declspec(novtable)
@@ -16,7 +16,7 @@
 */
 /* @@MIDL_FILE_HEADING(  ) */
 
-#if !defined(_M_IA64) && !defined(_M_AMD64) && !defined(_ARM_)
+#if defined(_M_AMD64)
 
 
 #if _MSC_VER >= 1200
@@ -26,15 +26,12 @@
 #pragma warning( disable: 4211 )  /* redefine extern to static */
 #pragma warning( disable: 4232 )  /* dllimport identity*/
 #pragma warning( disable: 4024 )  /* array to pointer mapping*/
-#pragma warning( disable: 4100 ) /* unreferenced arguments in x86 call */
-
-#pragma optimize("", off ) 
 
 #include <string.h>
 #include "Example1_h.h"
 
 #define TYPE_FORMAT_STRING_SIZE   7                                 
-#define PROC_FORMAT_STRING_SIZE   31                                
+#define PROC_FORMAT_STRING_SIZE   33                                
 #define EXPR_FORMAT_STRING_SIZE   1                                 
 #define TRANSMIT_AS_TABLE_SIZE    0            
 #define WIRE_MARSHAL_TABLE_SIZE   0            
@@ -61,6 +58,10 @@ typedef struct _Example1_MIDL_EXPR_FORMAT_STRING
 static const RPC_SYNTAX_IDENTIFIER  _RpcTransferSyntax = 
 {{0x8A885D04,0x1CEB,0x11C9,{0x9F,0xE8,0x08,0x00,0x2B,0x10,0x48,0x60}},{2,0}};
 
+static const RPC_SYNTAX_IDENTIFIER  _NDR64_RpcTransferSyntax = 
+{{0x71710533,0xbeba,0x4937,{0x83,0x19,0xb5,0xdb,0xef,0x9c,0xcc,0x36}},{1,0}};
+
+
 extern const Example1_MIDL_TYPE_FORMAT_STRING Example1__MIDL_TypeFormatString;
 extern const Example1_MIDL_PROC_FORMAT_STRING Example1__MIDL_ProcFormatString;
 extern const Example1_MIDL_EXPR_FORMAT_STRING Example1__MIDL_ExprFormatString;
@@ -83,23 +84,16 @@ static const RPC_SERVER_INTERFACE Example1___RpcServerInterface =
     0,
     0,
     &Example1_ServerInfo,
-    0x04000000
+    0x06000000
     };
 RPC_IF_HANDLE Example1_v1_0_s_ifspec = (RPC_IF_HANDLE)& Example1___RpcServerInterface;
 
 extern const MIDL_STUB_DESC Example1_StubDesc;
 
 
-#if !defined(__RPC_WIN32__)
+#if !defined(__RPC_WIN64__)
 #error  Invalid build platform for this stub.
 #endif
-#if !(TARGET_IS_NT60_OR_LATER)
-#error You need Windows Vista or later to run this stub because it uses these features:
-#error   compiled for Windows Vista.
-#error However, your C/C++ compilation flags indicate you intend to run this app on earlier systems.
-#error This app will fail with the RPC_X_WRONG_STUB_VERSION error.
-#endif
-
 
 static const Example1_MIDL_PROC_FORMAT_STRING Example1__MIDL_ProcFormatString =
     {
@@ -112,22 +106,23 @@ static const Example1_MIDL_PROC_FORMAT_STRING Example1__MIDL_ProcFormatString =
 			0x48,		/* Old Flags:  */
 /*  2 */	NdrFcLong( 0x0 ),	/* 0 */
 /*  6 */	NdrFcShort( 0x0 ),	/* 0 */
-/*  8 */	NdrFcShort( 0x4 ),	/* x86 Stack size/offset = 4 */
+/*  8 */	NdrFcShort( 0x8 ),	/* X64 Stack size/offset = 8 */
 /* 10 */	NdrFcShort( 0x0 ),	/* 0 */
 /* 12 */	NdrFcShort( 0x0 ),	/* 0 */
 /* 14 */	0x42,		/* Oi2 Flags:  clt must size, has ext, */
 			0x1,		/* 1 */
-/* 16 */	0x8,		/* 8 */
+/* 16 */	0xa,		/* 10 */
 			0x1,		/* Ext Flags:  new corr desc, */
 /* 18 */	NdrFcShort( 0x0 ),	/* 0 */
 /* 20 */	NdrFcShort( 0x0 ),	/* 0 */
 /* 22 */	NdrFcShort( 0x0 ),	/* 0 */
+/* 24 */	NdrFcShort( 0x0 ),	/* 0 */
 
 	/* Parameter szOutput */
 
-/* 24 */	NdrFcShort( 0x10b ),	/* Flags:  must size, must free, in, simple ref, */
-/* 26 */	NdrFcShort( 0x0 ),	/* x86 Stack size/offset = 0 */
-/* 28 */	NdrFcShort( 0x4 ),	/* Type Offset=4 */
+/* 26 */	NdrFcShort( 0x10b ),	/* Flags:  must size, must free, in, simple ref, */
+/* 28 */	NdrFcShort( 0x0 ),	/* X64 Stack size/offset = 0 */
+/* 30 */	NdrFcShort( 0x4 ),	/* Type Offset=4 */
 
 			0x0
         }
@@ -154,6 +149,159 @@ static const unsigned short Example1_FormatStringOffsetTable[] =
     };
 
 
+static const RPC_DISPATCH_FUNCTION Example1_table[] =
+    {
+    NdrServerCall2,
+    0
+    };
+static const RPC_DISPATCH_TABLE Example1_v1_0_DispatchTable = 
+    {
+    1,
+    (RPC_DISPATCH_FUNCTION*)Example1_table
+    };
+
+
+#endif /* defined(_M_AMD64)*/
+
+
+
+/* this ALWAYS GENERATED file contains the RPC server stubs */
+
+
+ /* File created by MIDL compiler version 8.01.0622 */
+/* at Mon Jan 18 19:14:07 2038
+ */
+/* Compiler settings for Example1.idl:
+    Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 8.01.0622 
+    protocol : all , ms_ext, app_config, c_ext, robust
+    error checks: allocation ref bounds_check enum stub_data 
+    VC __declspec() decoration level: 
+         __declspec(uuid()), __declspec(selectany), __declspec(novtable)
+         DECLSPEC_UUID(), MIDL_INTERFACE()
+*/
+/* @@MIDL_FILE_HEADING(  ) */
+
+#if defined(_M_AMD64)
+
+
+
+
+#if !defined(__RPC_WIN64__)
+#error  Invalid build platform for this stub.
+#endif
+
+
+#include "ndr64types.h"
+#include "pshpack8.h"
+
+
+typedef 
+struct _NDR64_CONFORMANT_STRING_FORMAT
+__midl_frag4_t;
+extern const __midl_frag4_t __midl_frag4;
+
+typedef 
+struct _NDR64_POINTER_FORMAT
+__midl_frag3_t;
+extern const __midl_frag3_t __midl_frag3;
+
+typedef 
+struct 
+{
+    struct _NDR64_PROC_FORMAT frag1;
+    struct _NDR64_PARAM_FORMAT frag2;
+}
+__midl_frag2_t;
+extern const __midl_frag2_t __midl_frag2;
+
+typedef 
+NDR64_FORMAT_UINT32
+__midl_frag1_t;
+extern const __midl_frag1_t __midl_frag1;
+
+static const __midl_frag4_t __midl_frag4 =
+{ 
+/* *char */
+    { 
+    /* *char */
+        0x63,    /* FC64_CONF_CHAR_STRING */
+        { 
+        /* *char */
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0
+        },
+        (NDR64_UINT16) 1 /* 0x1 */
+    }
+};
+
+static const __midl_frag3_t __midl_frag3 =
+{ 
+/* *char */
+    0x20,    /* FC64_RP */
+    (NDR64_UINT8) 0 /* 0x0 */,
+    (NDR64_UINT16) 0 /* 0x0 */,
+    &__midl_frag4
+};
+
+static const __midl_frag2_t __midl_frag2 =
+{ 
+/* Output */
+    { 
+    /* Output */      /* procedure Output */
+        (NDR64_UINT32) 262210 /* 0x40042 */,    /* primitive handle */ /* IsIntrepreted, ClientMustSize */
+        (NDR64_UINT32) 8 /* 0x8 */ ,  /* Stack size */
+        (NDR64_UINT32) 0 /* 0x0 */,
+        (NDR64_UINT32) 0 /* 0x0 */,
+        (NDR64_UINT16) 0 /* 0x0 */,
+        (NDR64_UINT16) 0 /* 0x0 */,
+        (NDR64_UINT16) 1 /* 0x1 */,
+        (NDR64_UINT16) 0 /* 0x0 */
+    },
+    { 
+    /* szOutput */      /* parameter szOutput */
+        &__midl_frag4,
+        { 
+        /* szOutput */
+            1,
+            1,
+            0,
+            1,
+            0,
+            0,
+            0,
+            0,
+            1,
+            0,
+            0,
+            0,
+            0,
+            (NDR64_UINT16) 0 /* 0x0 */,
+            0
+        },    /* MustSize, MustFree, [in], SimpleRef */
+        (NDR64_UINT16) 0 /* 0x0 */,
+        0 /* 0x0 */,   /* Stack offset */
+    }
+};
+
+static const __midl_frag1_t __midl_frag1 =
+(NDR64_UINT32) 0 /* 0x0 */;
+
+
+#include "poppack.h"
+
+
+static const FormatInfoRef Example1_Ndr64ProcTable[] =
+    {
+    &__midl_frag2
+    };
+
+
 static const MIDL_STUB_DESC Example1_StubDesc = 
     {
     (void *)& Example1___RpcServerInterface,
@@ -172,22 +320,47 @@ static const MIDL_STUB_DESC Example1_StubDesc =
     0,
     0,
     0,  /* notify & notify_flag routine table */
-    0x1, /* MIDL flag */
+    0x2000001, /* MIDL flag */
     0, /* cs routines */
-    0,   /* proxy/server info */
+    (void *)& Example1_ServerInfo,   /* proxy/server info */
     0
     };
 
-static const RPC_DISPATCH_FUNCTION Example1_table[] =
+static const RPC_DISPATCH_FUNCTION Example1_NDR64__table[] =
     {
-    NdrServerCall2,
+    NdrServerCallAll,
     0
     };
-static const RPC_DISPATCH_TABLE Example1_v1_0_DispatchTable = 
+static const RPC_DISPATCH_TABLE Example1_NDR64__v1_0_DispatchTable = 
     {
     1,
-    (RPC_DISPATCH_FUNCTION*)Example1_table
+    (RPC_DISPATCH_FUNCTION*)Example1_NDR64__table
     };
+
+static const MIDL_SYNTAX_INFO Example1_SyntaxInfo [  2 ] = 
+    {
+    {
+    {{0x8A885D04,0x1CEB,0x11C9,{0x9F,0xE8,0x08,0x00,0x2B,0x10,0x48,0x60}},{2,0}},
+    (RPC_DISPATCH_TABLE*)&Example1_v1_0_DispatchTable,
+    Example1__MIDL_ProcFormatString.Format,
+    Example1_FormatStringOffsetTable,
+    Example1__MIDL_TypeFormatString.Format,
+    0,
+    0,
+    0
+    }
+    ,{
+    {{0x71710533,0xbeba,0x4937,{0x83,0x19,0xb5,0xdb,0xef,0x9c,0xcc,0x36}},{1,0}},
+    (RPC_DISPATCH_TABLE*)&Example1_NDR64__v1_0_DispatchTable,
+    0 ,
+    (unsigned short *) Example1_Ndr64ProcTable,
+    0,
+    0,
+    0,
+    0
+    }
+    };
+
 
 static const SERVER_ROUTINE Example1_ServerRoutineTable[] = 
     {
@@ -199,16 +372,16 @@ static const MIDL_SERVER_INFO Example1_ServerInfo =
     &Example1_StubDesc,
     Example1_ServerRoutineTable,
     Example1__MIDL_ProcFormatString.Format,
-    Example1_FormatStringOffsetTable,
+    (unsigned short *) Example1_FormatStringOffsetTable,
     0,
-    0,
-    0,
-    0};
-#pragma optimize("", on )
+    (RPC_SYNTAX_IDENTIFIER*)&_NDR64_RpcTransferSyntax,
+    2,
+    (MIDL_SYNTAX_INFO*)Example1_SyntaxInfo
+    };
 #if _MSC_VER >= 1200
 #pragma warning(pop)
 #endif
 
 
-#endif /* !defined(_M_IA64) && !defined(_M_AMD64) && !defined(_ARM_) */
+#endif /* defined(_M_AMD64)*/
 
